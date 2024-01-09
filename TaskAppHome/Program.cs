8a5a2fbf325c854b2f2265f2d26bb45f74bc0a91
Eldorado009 <eldarna@code.edu.az>
2024-01-09 05:40:04 +0400

    First commit

---
+++ TaskAppHome/Program.cs
@@ -0,0 +1,14 @@
+﻿string Welcome = "  _      _________________  __  _________\r\n | | /| / / __/ / ___/ __ \\/  |/  / __/ /\r\n | |/ |/ / _// / /__/ /_/ / /|_/ / _//_/ \r\n |__/|__/___/_/\\___/\\____/_/  /_/___(_)  \r\n                                         ";
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(Welcome);
+string Option = "Option download:)";
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(Option);
+Console.ResetColor();
+
+
+Directory.CreateDirectory(@".\Product");
+if (!File.Exists(@".\Product\product.txt"))
+{
+    File.Create(@".\Product\product.txt");
+}
